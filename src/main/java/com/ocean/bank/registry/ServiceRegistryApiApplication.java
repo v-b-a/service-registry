6d294b0f44e809880bafe7d42155bf36ee9affbd
Валиахмедов Бари <BValiakhmedov@alfabank.ru>
2023-10-22 15:56:43 +0300

    init

---
+++ src/main/java/com/ocean/bank/registry/ServiceRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package com.ocean.bank.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegistryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ocean/bank/registry/ServiceRegistryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocean.bank.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
